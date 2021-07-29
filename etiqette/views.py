f4b500956c791037bf1750b4855b3ef26b29837b
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-29 11:37:25 +0300

    Movie card design complete.

--- etiqette/views.py
+++ etiqette/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
 
     return render(request, 'etiqette/index.html')
+
+def single_movie(request):
+
+    return render(request, 'etiqette/movie.html')
