421cc95eedcdb1d547982fdc85d856086c962b3b
Peter Kennedy <peter.kimani@student.moringaschool.com>
2021-07-30 01:32:35 +0300

    Register models to admin.

--- etiqette/admin.py
+++ etiqette/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Profile, Ticket, Cinema, Session
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Ticket)
+admin.site.register(Cinema)
+admin.site.register(Session)
